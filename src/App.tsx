df0cbbdfe94cfee251245226d063258c6c7af7eb
Kamil <kamil.bandzwolek@gmail.com>
2022-12-17 12:27:47 +0100

    V003
    Czystka i dodanie TS

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+//@ts-check
+import * as React from 'react';
+import './styles/App.css';
+
+
+function App() {
+  return(
+    <div className="App">
+      <header>
+        <h1 className="App-title1">Collective Goerli Faucet</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+//@ts-check
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
